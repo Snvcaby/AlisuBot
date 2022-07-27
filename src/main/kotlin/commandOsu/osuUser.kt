619b2ea44d291e49bee526b3bc43fc9c19eac0e5
IroSorA <894646267@qq.com>
2022-07-27 19:19:29 +0800

    bind暂时只能静态输入，因此暂时搁置

--- src/main/kotlin/commandOsu/osuUser.kt
+++ src/main/kotlin/commandOsu/osuUser.kt
@@ -10,6 +10,7 @@ import config.osuConfig.modeInSearch
 import config.osuConfig.osuAPI_key
 import utils.JsonUtil
 import xyz.irosoralive.bean.osuUserBean
+import xyz.irosoralive.commandOsu.osuUser.userinfo
 import xyz.irosoralive.data.osuBindData.bindMap
 import java.net.URL
 
@@ -49,23 +50,58 @@ object osuUser : CompositeCommand(
         }
     }
 
-    @SubCommand("bind")
-    @Description("qq号与用户名绑定")
-    suspend fun CommandSender.userBind(username: String){
-        try {
-            val qq:Long = (this as CommandSenderOnMessage<*>).user!!.id
-
-            sendMessage((this as CommandSenderOnMessage<*>).fromEvent.source.quote() + "qq是${qq}")
-
-            if(bindMap[qq] ==null){
-                bindMap += Pair(qq,username)
-                sendMessage((this as CommandSenderOnMessage<*>).fromEvent.source.quote() + bindMap.toString())
-            }
-
-        }catch (e: Exception){
-            e.printStackTrace()
-        }
-    }
+//    @SubCommand("self")
+//    @Description("查询自身用户信息")
+//    suspend fun CommandSender.userinfoSelf(){
+//        try {
+//            val id = bindMap.get((this as CommandSenderOnMessage<*>).user!!.id)
+//            if (id != null){
+//                val username: String? = bindMap[id.toLong()]
+//                val url = "https://osu.ppy.sh/api/get_user?k=${osuAPI_key}&u=${username}&m=${modeInSearch}"
+//                val userJsonS: String = URL(url).readText().drop(1).dropLast(1)
+//                if (userJsonS.equals("\"error\":\"Please provide a valid API key.\"")) {
+//                    sendMessage((this as CommandSenderOnMessage<*>).fromEvent.source.quote() + "非法API，请核对配置：osuConfig中的osuAPI_key是否填写错误。")
+//                } else if (modeInSearch != 0 && modeInSearch != 1 && modeInSearch != 2 && modeInSearch != 3) {
+//                    sendMessage((this as CommandSenderOnMessage<*>).fromEvent.source.quote() + "非法的模式，请核对配置：osuConfig中的modeInSearch是否填写错误")
+//                } else {
+//                    val user: osuUserBean = Gson().fromJson(userJsonS, osuUserBean::class.java)
+//                    when (userJsonS) {
+//                        "" -> sendMessage((this as CommandSenderOnMessage<*>).fromEvent.source.quote() + "用户不存在")
+//                        else -> {
+//                            val out = JsonUtil.findUser(user)
+//                            sendMessage((this as CommandSenderOnMessage<*>).fromEvent.source.quote() + out)
+//                        }
+//
+//                    }
+//                }
+//
+//            }else{
+//                sendMessage("没有绑定osu用户名，请先用 /osu! bind [user] 绑定用户名")
+//            }
+//        }catch (e:Exception){
+//            e.printStackTrace()
+//        }
+//
+//    }
+//
+//    @SubCommand("bind")
+//    @Description("qq号与用户名绑定")
+//    suspend fun CommandSender.userBind(username: String){
+//        try {
+//            val qq:Long = (this as CommandSenderOnMessage<*>).user!!.id
+//
+//
+//            if(bindMap[qq] ==null){
+//                bindMap += Pair(qq,username)
+//                sendMessage((this as CommandSenderOnMessage<*>).fromEvent.source.quote() + "绑定成功:${qq} -> $username")
+//            }else{
+//                sendMessage((this as CommandSenderOnMessage<*>).fromEvent.source.quote() + "失败:已绑定")
+//            }
+//
+//        }catch (e: Exception){
+//            e.printStackTrace()
+//        }
+//    }
 
     //todo
     //preview
