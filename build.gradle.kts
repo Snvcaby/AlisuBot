fbe62b86ee071d3eeea8fc425d0565490486ff08
kkry <894646267@qq.com>
2024-03-20 16:05:46 +0800

    添加消息缓存。本次提交将引发CVE-2024-1597漏洞。等待前置插件更新以修复漏洞。

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    val kotlinVersion = "1.5.10"
+    val kotlinVersion = "1.8.0"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.serialization") version kotlinVersion
 
@@ -11,6 +11,11 @@ version = "0.1B"
 
 dependencies {
     api("com.google.code.gson:gson:2.9.0")
+    compileOnly("xyz.cssxsh.mirai:mirai-hibernate-plugin:2.8.0")
+}
+
+mirai {
+    jvmTarget = JavaVersion.VERSION_11
 }
 
 repositories {
